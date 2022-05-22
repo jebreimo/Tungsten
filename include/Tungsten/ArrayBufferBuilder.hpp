5ff1f09e2473a7438a92e927e81f4ffe47d63277
Jan Erik Breimo <jebreimo@gmail.com>
2022-05-22 21:57:34 +0200

    Make ArrayBufferBuilder methods chainable.

--- include/Tungsten/ArrayBufferBuilder.hpp
+++ include/Tungsten/ArrayBufferBuilder.hpp
@@ -26,14 +26,16 @@ namespace Tungsten
               base_index_(base_index)
         {}
 
-        void reserve_vertexes(size_t count)
+        ArrayBufferBuilder& reserve_vertexes(size_t count)
         {
             array_.vertexes.reserve(base_index_ + count);
+            return *this;
         }
 
-        void add_vertex(const Item& vertex)
+        ArrayBufferBuilder& add_vertex(const Item& vertex)
         {
             array_.vertexes.push_back(vertex);
+            return *this;
         }
 
         [[nodiscard]]
@@ -42,21 +44,24 @@ namespace Tungsten
             return array_.vertexes[index];
         }
 
-        void reserve_indexes(size_t count)
+        ArrayBufferBuilder& reserve_indexes(size_t count)
         {
             array_.indexes.reserve(array_.indexes.size() + count);
+            return *this;
         }
 
-        void add_index(uint16_t a)
+        ArrayBufferBuilder& add_index(uint16_t a)
         {
             array_.indexes.push_back(a + base_index_);
+            return *this;
         }
 
-        void add_indexes(uint16_t a, uint16_t b, uint16_t c)
+        ArrayBufferBuilder& add_indexes(uint16_t a, uint16_t b, uint16_t c)
         {
             array_.indexes.push_back(a + base_index_);
             array_.indexes.push_back(b + base_index_);
             array_.indexes.push_back(c + base_index_);
+            return *this;
         }
     private:
         ArrayBuffer<Item>& array_;
